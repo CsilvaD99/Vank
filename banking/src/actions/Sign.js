9ba84431b5f4fb148e893209394326af69925786
CsilvaD99 <Carlos.silva.clss17@gmail.com>
2022-09-26 11:10:50 -0500

    /api

--- banking/src/actions/Sign.js
+++ banking/src/actions/Sign.js
@@ -10,16 +10,13 @@ export const SignnUp = async (
   const body = { email, password, firstName, lastName, role, address };
   console.log("sinnup info", body);
   try {
-    await fetch(
-      `https://6331bf4db89ad909bc1db7ad--nimble-druid-24ce06.netlify.app/user/signupuser`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(body),
-      }
-    );
+    await fetch(`/api/user/signupuser`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
     console.log("body after request", body);
     console.log("signupworked");
   } catch (error) {
@@ -31,16 +28,13 @@ export const LogInn = async (email, password) => {
   const body = { email, password };
   console.log("body", body);
   try {
-    await fetch(
-      `https://6331bf4db89ad909bc1db7ad--nimble-druid-24ce06.netlify.app/user/signin`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(body),
-      }
-    );
+    await fetch(`/api/user/signin`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
     console.log("body stringify", JSON.stringify(body));
   } catch (error) {
     console.log("LOG IN ERROR", error);
@@ -48,12 +42,9 @@ export const LogInn = async (email, password) => {
 };
 export const LogOutt = async () => {
   try {
-    await fetch(
-      `https://6331bf4db89ad909bc1db7ad--nimble-druid-24ce06.netlify.app/user/logout`,
-      {
-        method: "GET",
-      }
-    );
+    await fetch(`/api/user/logout`, {
+      method: "GET",
+    });
     console.log("userlogged out");
   } catch (error) {
     console.log(error);
